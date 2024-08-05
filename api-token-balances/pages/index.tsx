77e553d6178f3979df7352d6f976df93db6bbb4c
emmaodia <odia.emma@gmail.com>
2024-08-05 14:03:01 +0100

    Endpoint Updated.

--- api-token-balances/pages/index.tsx
+++ api-token-balances/pages/index.tsx
@@ -5,42 +5,37 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // 0xa42872B5359F6e3905BB031df62C3AADde532933
+  // Example Address: 0x07dc9cb1d2f8e7acf92c856cf43467936203f26a
   const [address, setAddress] = useState("");
   const [tokenData, setTokenData] = useState([]);
 
+  const apiUrl = `https://api.fuse.io/api/v0/balances/assets/${address}?apiKey={API_KEY}`;
+  //[Get an API KEY](https://console.fuse.io/build).
 
-  const apiUrl = `https://api.fuse.io/api/v0/explorer?module=account&action=tokenlist&apiKey=pk_API_KEY&address=${address}`
-  
   const handleReturnBalances = async (e) => {
     e.preventDefault();
     fetchData();
   };
 
-  
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
   async function fetchData() {
     try {
-      console.log(address)
+      console.log(address);
       const response = await fetch(apiUrl);
-      console.log(address)
+      console.log(address);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       console.log(data);
       setTokenData(data.result);
     } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error("There was a problem with the fetch operation:", error);
     }
   }
 
   return (
     <main className={`flex min-h-screen flex-col p-24 ${inter.className}`}>
-       <form
+      <form
         onSubmit={handleReturnBalances}
         className="flex max-w-md flex-col gap-4"
       >
@@ -67,38 +62,41 @@ export default function Home() {
           Send
         </button>
       </form>
-        <div className="mt-12 relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                            Token Symbol
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                             Contract Address
-                        </th>
-                        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                            Balance
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Token Type
-                        </th>
-                    </tr>
-                </thead>
+      <div className="mt-8 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="mb-4">Address: {address}</div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Token Symbol
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Contract Address
+              </th>
+              <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Balance
+              </th>
+            </tr>
+          </thead>
           <tbody>
             {tokenData.map((token, index) => (
-              <tr key={index} className={`${index % 2 === 0 ? 'border-b border-gray-200 dark:border-gray-700' : ''}`}>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+              <tr
+                key={index}
+                className={`${
+                  index % 2 === 0
+                    ? "border-b border-gray-200 dark:border-gray-700"
+                    : ""
+                }`}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                >
                   {token.symbol}
                 </th>
-                <td className="px-6 py-4">
-                  {token.contractAddress}
-                </td>
+                <td className="px-6 py-4">{token.contractAddress}</td>
                 <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                   {token.balance}
-                </td>
-                 <td className="px-6 py-4">
-                  {token.type}
                 </td>
               </tr>
             ))}
